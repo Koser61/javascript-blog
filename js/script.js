04b8901afd85f7a900f081b6d852cb3417fd2532
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-05-05 14:56:34 +0200

    Add mobile menu script

--- js/script.js
+++ js/script.js
@@ -72,4 +72,56 @@ function generateTitleLinks(){
     }
 } 
 
+const buttonHamburger = document.getElementById('hamburger');
+
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  if (mobileMenu.style.display === 'none') {
+    mobileMenu.style.display = 'flex';
+    buttonHamburger.classList.add('active');
+  } 
+  else {
+    mobileMenu.style.display = 'none';
+    buttonHamburger.classList.remove('active');
+  }
+}
+
+const divAuthors = document.getElementById('mobile-authors');
+
+function toggleAuthorList() {
+  const divAuthorList = document.getElementById('mobile-author-list');
+  if (divAuthorList.style.display === 'none') {
+    divAuthorList.style.display = 'flex';
+    divAuthors.classList.add('active');
+  }
+  else {
+    divAuthorList.style.display = 'none';
+    divAuthors.classList.remove('active');
+  }
+}
+
+const divTitles = document.getElementById('mobile-titles');
+
+function toggleTitleList() {
+  const divTitleList = document.getElementById('mobile-title-list');
+  if (divTitleList.style.display === 'none') {
+    divTitleList.style.display = 'flex';
+    divTitles.classList.add('active');
+  }
+  else {
+    divTitleList.style.display = 'none';
+    divTitles.classList.remove('active');
+  }
+}
+
 generateTitleLinks();
+
+buttonHamburger.addEventListener('click', function (){
+  toggleMobileMenu();
+});
+divAuthors.addEventListener('click', function (){
+  toggleAuthorList();
+});
+divTitles.addEventListener('click', function (){
+  toggleTitleList();
+});
