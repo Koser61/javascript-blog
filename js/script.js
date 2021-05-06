3a5f4e8ba04204dd1f0cba01e17b165dbbbaa715
Piotr Kosiński <kosinskipiotr.98@gmail.com>
2021-05-06 15:40:06 +0200

    Changed back HTML structure & Fix RWD layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const buttonHamburger = document.getElementById('hamburger'),
+  optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
@@ -33,19 +38,14 @@ function titleClickHandler(event){
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
   }
-  
-const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
   /* [DONE] remove contents of titleList */
-  const titleList = document.querySelector(optTitleListSelector)
+  const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
   /* [DONE] find all the articles and save them to variable: articles */
   const articles = document.querySelectorAll(optArticleSelector);
-
   let html = '';
 
   for(let article of articles){
@@ -70,58 +70,11 @@ function generateTitleLinks(){
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
     }
+    console.log(titleList);
 } 
-
-const buttonHamburger = document.getElementById('hamburger');
-
-function toggleMobileMenu() {
-  const mobileMenu = document.getElementById('mobile-menu');
-  if (mobileMenu.style.display === 'none') {
-    mobileMenu.style.display = 'flex';
-    buttonHamburger.classList.add('active');
-  } 
-  else {
-    mobileMenu.style.display = 'none';
-    buttonHamburger.classList.remove('active');
-  }
-}
-
-const divAuthors = document.getElementById('mobile-authors');
-
-function toggleAuthorList() {
-  const divAuthorList = document.getElementById('mobile-author-list');
-  if (divAuthorList.style.display === 'none') {
-    divAuthorList.style.display = 'flex';
-    divAuthors.classList.add('active');
-  }
-  else {
-    divAuthorList.style.display = 'none';
-    divAuthors.classList.remove('active');
-  }
-}
-
-const divTitles = document.getElementById('mobile-titles');
-
-function toggleTitleList() {
-  const divTitleList = document.getElementById('mobile-title-list');
-  if (divTitleList.style.display === 'none') {
-    divTitleList.style.display = 'flex';
-    divTitles.classList.add('active');
-  }
-  else {
-    divTitleList.style.display = 'none';
-    divTitles.classList.remove('active');
-  }
-}
 
 generateTitleLinks();
 
 buttonHamburger.addEventListener('click', function (){
-  toggleMobileMenu();
-});
-divAuthors.addEventListener('click', function (){
-  toggleAuthorList();
-});
-divTitles.addEventListener('click', function (){
-  toggleTitleList();
+  
 });
